fd2aff25e0235e7c89b1f62a56d2304276aac775
kampffrosch <kampffrosch@weg-werf-email.de>
2015-01-28 00:07:10 +0100

    Functioning camera capable of taking exactly one picture

--- MorseApp/src/de/tu/dresden/morseapp/MainActivity.java
+++ MorseApp/src/de/tu/dresden/morseapp/MainActivity.java
@@ -1,36 +1,69 @@
 package de.tu.dresden.morseapp;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+
+//see http://www.tutorialspoint.com/android/android_camera.htm for the source
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+   private Camera cameraObject;
+   private ShowCamera showCamera;
+   private ImageView pic;
+   public static Camera isCameraAvailiable(){
+      Camera object = null;
+      try {
+         object = Camera.open(); 
+      }
+      catch (Exception e){
+      }
+      return object; 
+   }
 
+   private PictureCallback capturedIt = new PictureCallback() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+      @Override
+      public void onPictureTaken(byte[] data, Camera camera) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+      Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
+      if(bitmap==null){
+         Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
+      }
+      else
+      {
+         Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
+      }
+      cameraObject.release();
+   }
+};
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      pic = (ImageView)findViewById(R.id.imageView1);
+      cameraObject = isCameraAvailiable();
+      showCamera = new ShowCamera(this, cameraObject);
+      FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+      preview.addView(showCamera);
+   }
+   public void snapIt(View view){
+      cameraObject.takePicture(null, null, capturedIt);
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
 }
