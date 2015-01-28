04879324ea632afc4ce463a8259c104c74303d2d
kampffrosch <kampffrosch@weg-werf-email.de>
2015-01-28 15:37:50 +0100

    Picture analysis => algorithm may have to be improved

--- MorseApp/src/de/tu/dresden/morseapp/MainActivity.java
+++ MorseApp/src/de/tu/dresden/morseapp/MainActivity.java
@@ -2,6 +2,7 @@ package de.tu.dresden.morseapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
@@ -12,58 +13,85 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-
 //see http://www.tutorialspoint.com/android/android_camera.htm for the source
 
 public class MainActivity extends Activity {
 
-   private Camera cameraObject;
-   private ShowCamera showCamera;
-   private ImageView pic;
-   public static Camera isCameraAvailiable(){
-      Camera object = null;
-      try {
-         object = Camera.open(); 
-      }
-      catch (Exception e){
-      }
-      return object; 
-   }
+	private Camera cameraObject;
+	private ShowCamera showCamera;
+	private ImageView pic;
 
-   private PictureCallback capturedIt = new PictureCallback() {
+	public static Camera isCameraAvailiable() {
+		Camera object = null;
+		try {
+			object = Camera.open();
+		} catch (Exception e) {
+		}
+		return object;
+	}
 
-      @Override
-      public void onPictureTaken(byte[] data, Camera camera) {
+	private PictureCallback capturedIt = new PictureCallback() {
 
-      Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
-      if(bitmap==null){
-         Toast.makeText(getApplicationContext(), "No Picture taken.", Toast.LENGTH_SHORT).show();
-      }
-      else
-      {
-         pic.setImageBitmap(bitmap);
-      }
-      cameraObject.startPreview();
-   }
-};
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
-      pic = (ImageView)findViewById(R.id.imageView1);
-      cameraObject = isCameraAvailiable();
-      showCamera = new ShowCamera(this, cameraObject);
-      FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-      preview.addView(showCamera);
-   }
-   public void snapIt(View view){
-      cameraObject.takePicture(null, null, capturedIt);
-   }
+			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+			if (bitmap == null) {
+				Toast.makeText(getApplicationContext(), "No Picture taken.",
+						Toast.LENGTH_SHORT).show();
+			} else {
+				pic.setImageBitmap(bitmap);
+				if (hasSignal(bitmap)) {
+					Toast.makeText(getApplicationContext(), "Signal!",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(getApplicationContext(), "No Signal.",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+			cameraObject.startPreview();
+		}
+	};
 
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      getMenuInflater().inflate(R.menu.main, menu);
-      return true;
-   }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		pic = (ImageView) findViewById(R.id.imageView1);
+		cameraObject = isCameraAvailiable();
+		showCamera = new ShowCamera(this, cameraObject);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(showCamera);
+	}
+
+	public void snapIt(View view) {
+		cameraObject.takePicture(null, null, capturedIt);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public boolean hasSignal(Bitmap bitmap) {
+		int whitecount = 0;
+		int pixel;
+		for (int x = 0; x < bitmap.getWidth(); x++) {
+			for (int y = 0; y < bitmap.getHeight(); y++) {
+				pixel = bitmap.getPixel(x, y);
+				if ((Color.green(pixel) > 240) && (Color.red(pixel) > 240)
+						&& (Color.blue(pixel) > 240)) {
+					whitecount++;
+				}
+			}
+		}
+
+		if (whitecount > 300) {
+			return true;
+		}
+
+		return false;
+
+	}
 }
