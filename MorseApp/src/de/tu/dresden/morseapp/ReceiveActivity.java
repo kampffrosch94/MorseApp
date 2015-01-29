0faa44d5821a2cd0a2df1915832b06841dcf9871
kampffrosch <kampffrosch@weg-werf-email.de>
2015-01-29 11:56:42 +0100

    camera and imageanalysis stuff now in seperate thread

--- MorseApp/src/de/tu/dresden/morseapp/ReceiveActivity.java
+++ MorseApp/src/de/tu/dresden/morseapp/ReceiveActivity.java
@@ -10,7 +10,10 @@ import android.graphics.Paint;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -24,37 +27,18 @@ public class ReceiveActivity extends Activity {
 	private Camera cameraObject;
 	private ShowCamera showCamera;
 	private ImageView pic;
+	private CameraHandlerThread mThread = null;
+	private Handler activityHandler;
 
-	private PictureCallback capturedIt = new PictureCallback() {
-
-		@Override
-		public void onPictureTaken(byte[] data, Camera camera) {
-
-			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-			bitmap = toGrayscale(bitmap);
-			if (bitmap == null) {
-				Toast.makeText(getApplicationContext(), "No Picture taken.",
-						Toast.LENGTH_SHORT).show();
-			} else {
-				pic.setImageBitmap(bitmap);
-				if (hasSignal(bitmap)) {
-					Toast.makeText(getApplicationContext(), "Signal!",
-							Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(getApplicationContext(), "No Signal.",
-							Toast.LENGTH_SHORT).show();
-				}
-			}
-			cameraObject.startPreview();
-		}
-	};
+	private static final String debugLabel = "MorseReceiverDebug";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		activityHandler = new Handler();
 		setContentView(R.layout.activity_receive);
 		pic = (ImageView) findViewById(R.id.imageView1);
-		cameraObject = Camera.open();
+		openCamera();
 		cameraObject.setDisplayOrientation(90);
 		showCamera = new ShowCamera(this, cameraObject);
 		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
@@ -119,4 +103,76 @@ public class ReceiveActivity extends Activity {
 		c.drawBitmap(bmpOriginal, 0, 0, paint);
 		return bmpGrayscale;
 	}
+
+	private void openCamera() {
+		if (mThread == null) {
+			mThread = new CameraHandlerThread();
+		}
+
+		synchronized (mThread) {
+			mThread.openCamera();
+		}
+	}
+
+	private void handleSignal(final Bitmap bitmap, final boolean signal) {
+		activityHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				pic.setImageBitmap(bitmap);
+				if (signal) {
+					Toast.makeText(getApplicationContext(), "Signal!",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(getApplicationContext(), "No Signal.",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+
+	private class CameraHandlerThread extends HandlerThread {
+		Handler mHandler = null;
+
+		CameraHandlerThread() {
+			super("CameraHandlerThread");
+			start();
+			mHandler = new Handler(getLooper());
+		}
+
+		synchronized void notifyCameraOpened() {
+			notify();
+		}
+
+		void openCamera() {
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					cameraObject = Camera.open();
+					notifyCameraOpened();
+				}
+			});
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Log.d(debugLabel, "Wait for Camera was interrupted"); 
+				//Waiting should be interrupted
+			}
+		}
+	}
+
+	private PictureCallback capturedIt = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+			bitmap = toGrayscale(bitmap);
+			if (bitmap == null) {
+				Log.d(debugLabel, "No Picture taken.");
+			} else {
+				handleSignal(bitmap,hasSignal(bitmap));
+			}
+			cameraObject.startPreview();
+		}
+	};
 }
